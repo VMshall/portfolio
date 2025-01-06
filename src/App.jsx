c8ed0008d86200fc15bd000f07f2fe28a529f845
sandipv2 <sandiipvarma@gmail.com>
2025-01-06 14:06:08 +0530

    Fixed missing error.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Academics from "./components/Academics";
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <Navbar />
       <Hero />
       <About />
-      <Academics />
       <Projects />
       <Skills />
     </>
